0e6672beea2878ca48d63e07eacdefed7b39b3b2
Nanachi <a@techz.jp>
2026-05-22 20:50:02 +0900

    Create App.jsx

---
+++ Day1/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return(
+    <div>
+      <p>count: {count}</p>
+      <button onClick={() => setCount((count) => count + 1)}> +1 </button>
+      <button onClick={() => setCount((count) => count - 1)}> -1 </button>
+      <button onClick={() => setCount((count) => count * 2)}> x2 </button>
+      <button onClick={() => setCount(0)}> リセット </button>
+      <button onClick={() => setCount((count) => Math.abs(count))}> 絶対値 </button> 
+    </div>
+  );
+}
